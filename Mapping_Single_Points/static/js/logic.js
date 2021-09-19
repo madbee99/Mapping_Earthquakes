a673d08b3b7526e7dd1df29cadf66b0fcc6aef31
madbee99 <madb99@comcast.net>
2021-09-19 11:16:19 -0700

    mapping single points update

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -8,7 +8,7 @@ let map = L.map("mapid").setView([34.0522, -118.2437], 14);
 //  Add a marker to the map for Los Angeles, California.
 L.circleMarker([34.0522, -118.2437], {
   radius: 300,
-  color: "black", 
+  color: "black",
   fillColor: '#ffffa1'
 }).addTo(map);
 
